b75b85f9ede544379fb2bfe476f47ba7b292e380
Parth Kotwal <parthkotwa07@gmail.com>
2025-10-20 16:13:00 -0700

    New docker compose

--- backend/eggly_backend/settings.py
+++ backend/eggly_backend/settings.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
-import dj_database_url
-import numpy
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-cd*91btc84qyxs=hz!t7bn)29s*rj2ya#l4p%8=4#!m1t_0pq8'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
